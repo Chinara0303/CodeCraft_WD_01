1d31f85c404d7ec77794667eb44286b9eda474a5
= <=>
2024-12-02 16:36:22 +0400

    initial

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+// Add 'scrolled' class when user scrolls down
+window.addEventListener("scroll", () => {
+    const navbar = document.querySelector(".navbar");
+    if (window.scrollY > 50) {
+      navbar.classList.add("scrolled");
+    } else {
+      navbar.classList.remove("scrolled");
+    }
+  });
+  
